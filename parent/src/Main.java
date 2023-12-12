09dc472638801c5f1d505825b67e031c707cddbd
prashant patil <prashant.patil@sarathisoftech.com>
2023-12-12 18:14:56 +0530

    printing student

--- parent/src/Main.java
+++ parent/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Student student=new Student(1,"prashant", 40);
-
+        System.out.println(student);
 
     }
 }
